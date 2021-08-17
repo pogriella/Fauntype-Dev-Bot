5f9a56072ff0c30dacfb7ac31fb9a55d708f5e3c
Garrett Simpson <gevansimpson@gmail.com>
2021-08-17 17:38:10 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const dotenv = require('dotenv');
+// const { token } = require('./config.json');
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+dotenv.config();
+
+client.once('ready', () => {
+	if (client.guilds.cache.size > 1) {
+		console.log('Ready and watching ' + client.guilds.cache.size + ' guilds');
+	}
+	else {
+		console.log('Ready and watching ' + client.guilds.cache.size + ' guild');
+	}
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+	const command = interaction.commandName.toLowerCase();
+
+	if (command === 'ping') {
+		await interaction.reply('Pong!');
+	}
+});
+
+client.login(process.env.TOKEN);
