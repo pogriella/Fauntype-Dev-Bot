f01912b46fc40bc553ddf0223a1cd96957bcbd57
Garrett Simpson <gevansimpson@gmail.com>
2021-08-17 20:32:31 -0400

    Bugfix

--- index.js
+++ index.js
@@ -17,9 +17,8 @@ client.once('ready', () => {
 
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
-	const command = interaction.commandName.toLowerCase();
 
-	if (command === 'ping') {
+	if (interaction.commandName === 'ping') {
 		await interaction.reply('Pong!');
 	}
 });
